85aafc08ed8b84cdffdb46ad5b08dbafd975ec30
shenyiban <707150922@qq.com>
2019-05-29 13:50:12 +0800

    Initial commit

---
+++ src/com/company/QuizTest.java
@@ -0,0 +1,84 @@
+package com.company;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+
+
+public class QuizTest {
+
+    public AppiumDriver driver;
+    private boolean isInstall = false;
+
+    public void startRecord () throws IOException{
+      //  Runtime rc = Runtime.getRuntime();
+       // rc.exec("cmd.exe /E:/Android/sdk/platform-tools adb shell screenrecord /sdcard/runCase.mp4");//录制屏幕运行视频
+    }
+
+    /**
+     *  开始启动
+     * @throws IOException
+     */
+    @Before
+    public void setUp() throws IOException{
+        // 启动appium
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName", "emulator-5554");//指定测试机
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("platformName", "Android");// 指定测试平台
+        capabilities.setCapability("platformVersion", "22");//平台版本
+
+        // 配置测试apk
+        capabilities.setCapability("appPackage", "com.example.quizactivity");//测试的包
+        capabilities.setCapability("appActivity", "com.example.quizactivity.QuizActivity");//测试的activity
+        capabilities.setCapability("sessionoverride", true);//每次启动时覆盖session，否则第二次后运行会报错不能新建session
+        capabilities.setCapability("unicodekeyboard", true);//设置键盘
+        capabilities.setCapability("resetkeyboard", false); //设置默认键盘为appium的键盘
+        driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        // 安装APK,如果真机设备已经安装，则不需要重新安装
+        if (isInstall){
+            File classpathRoot = new File(System.getProperty("user.dir"));
+            File appDir = new File(classpathRoot,"apps");
+            File app = new File(appDir,"QuizActivity.apk");
+            capabilities.setCapability("app", app.getAbsolutePath());
+        }
+        startRecord();
+
+    }
+
+    /**
+     *  点击显示上一条
+     * @throws InterruptedException
+     */
+    @Test
+    public void PreQuestion() throws InterruptedException{
+        driver.findElementById("com.example.quizactivity:id/prev_button").click();// 点击上一个按钮
+        Thread.sleep(2000);
+        driver.findElementById("com.example.quizactivity:id/next_button").click();//点击下一个按钮
+        Thread.sleep(2000);
+        String tips = driver.findElementById("com.example.quizactivity:id/question_text_view").getText();
+        Assert.assertEquals(tips, "The Suez Canal connects the Red Sea and the Indian Ocean.");
+        Thread.sleep(2000);
+    }
+
+
+    /**
+     * 结束启动
+     */
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
