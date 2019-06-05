6cbee4556d3a52cdde8a4749b06e2c58d90a1fba
tiantan <707150922@qq.com>
2019-06-05 21:26:51 +0800

    test

--- src/com/company/QuizTest.java
+++ src/com/company/QuizTest.java
@@ -32,12 +32,12 @@ public class QuizTest {
      */
     @Before
     public void setUp() throws IOException{
-        // 启动appium gongsi
+      /*  // 启动appium gongsi
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("deviceName", "192.168.250.101:5555");//指定测试机
         capabilities.setCapability("automationName", "Appium");
         capabilities.setCapability("platformName", "Android");// 指定测试平台
-        capabilities.setCapability("platformVersion", "4.3");//平台版本*/
+        capabilities.setCapability("platformVersion", "4.3");//平台版本
 
         // 配置测试apk
         capabilities.setCapability("appPackage", "com.android.mms");//测试的包
@@ -47,9 +47,9 @@ public class QuizTest {
         capabilities.setCapability("resetkeyboard", false); //设置默认键盘为appium的键盘
         //driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         driver=new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);*/
 
-       /* // 启动appium suse
+        // 启动appium suse
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("deviceName", "192.168.67.101:5555");//指定测试机
         capabilities.setCapability("automationName", "Appium");
@@ -62,9 +62,8 @@ public class QuizTest {
         capabilities.setCapability("sessionoverride", true);//每次启动时覆盖session，否则第二次后运行会报错不能新建session
         capabilities.setCapability("unicodekeyboard", true);//设置键盘
         capabilities.setCapability("resetkeyboard", false); //设置默认键盘为appium的键盘
-        driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);*/
-
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
        /* // 安装APK,如果真机设备已经安装，则不需要重新安装
         if (isInstall){
             File classpathRoot = new File(System.getProperty("user.dir"));
@@ -83,14 +82,14 @@ public class QuizTest {
     @Test
     public void PreQuestion() throws InterruptedException{
         Thread.sleep(2000);
-       driver.findElementById("com.android.mms:id/action_compose_new").click();// 点击上一个按钮
+      // driver.findElementById("com.android.mms:id/action_compose_new").click();// 点击上一个按钮
       //  driver.findElementByAccessibilityId("action_compose_new").click();
         Thread.sleep(2000);
-       /* driver.findElementById("com.example.quizactivity:id/next_button").click();//点击下一个按钮
+       driver.findElementById("com.example.androidunittest:id/btnLog").click();//点击下一个按钮
         Thread.sleep(2000);
-        String tips = driver.findElementById("com.example.quizactivity:id/question_text_view").getText();
-        Assert.assertEquals(tips, "The Suez Canal connects the Red Sea and the Indian Ocean.");
-        Thread.sleep(2000);*/
+      //  String tips = driver.findElementById("com.example.quizactivity:id/question_text_view").getText();
+      //  Assert.assertEquals(tips, "The Suez Canal connects the Red Sea and the Indian Ocean.");
+        Thread.sleep(2000);
     }
 
 
